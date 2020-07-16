8389a6a05a914abb6d9333ca0b0d349db55c1e81
Prajakta <prajakta.rajapure@thoughtworks.com>
2020-07-17 00:24:44 +0530

    Initial commit

---
+++ src/test/kotlin/com/learning/springKotlin/SpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learning.springKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
